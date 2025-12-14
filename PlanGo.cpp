436e0637c808de648bdc96d53394ec9bc841d28b
Marc Ortiz González <marcortizgonzalez2002@gmail.com>
2025-12-14 12:27:03 +0100

    Main de prueba

--- PlanGo.cpp
+++ PlanGo.cpp
@@ -1,21 +1,40 @@
-// Marc Ortiz, Eduard Pernia, Sergio Fernandez
-// PlanGo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "CapaDeDomini.hxx"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+void mostrarMenu() {
+    std::cout << "1. Consultar Novetats" << std::endl;
+    std::cout << "2. Esborrar Usuari" << std::endl;
+    std::cout << "0. Sortir" << std::endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    CapaDeDomini::getInstance().iniciarSessio("marcortiz");
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    int opcio = -1;
+    while (opcio != 0) {
+        mostrarMenu();
+        std::cin >> opcio;
+
+        if (opcio == 1) {
+            auto novetats = CapaDeDomini::getInstance().consultarNovetats();
+            std::cout << "--- NOVETATS ---" << std::endl;
+            for (const auto& dto : novetats) {
+                std::cout << dto.tipus << ": " << dto.nom << " (" << dto.detalls << ")" << std::endl;
+            }
+        }
+        else if (opcio == 2) {
+            std::string pwd;
+            std::cout << "Password: ";
+            std::cin >> pwd;
+            try {
+                CapaDeDomini::getInstance().esborrarUsuari(pwd);
+                std::cout << "Esborrat!" << std::endl;
+            }
+            catch (std::exception& e) {
+                std::cout << "Error: " << e.what() << std::endl;
+            }
+        }
+    }
+    return 0;
+}
